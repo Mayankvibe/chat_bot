0ad6fffc4baa5cf6aca3ebfc0c511f7c3f220e60
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-03-22 11:26:11 +0530

    second  commit

--- chat_bot.py
+++ chat_bot.py
@@ -5,15 +5,28 @@ load_dotenv()
 from langchain_mistralai import ChatMistralAI
 from langchain_core.messages import AIMessage ,HumanMessage,SystemMessage
 
-model=ChatMistralAI(model="mistral-small-2506",temperature=0.9)
+model=ChatMistralAI(model="mistral-small-2506")
+
+user=int(input("Choice the number(1 for angry mode , 2 for joyful mode , 3 for sad mode): "))
+match user:
+ case 1:
+    mode="You are an angry AI agent. You respond aggressively and impatiently"
+ case 2:
+    mode=   "You are an enjoyful AI agent. You respond joyful and enjoyful"    
+ case 3:
+    mode=  "You are an sad AI agent. You respond sadly and painful"
+ case _:
+    print("enter valid number : ")        
+print("to exit press 0")
 messages=[
-    SystemMessage("You are an angry AI agent. You respond aggressively and impatiently")
-]
-print("to exit press 1")
+   SystemMessage(content=mode)
+]        
+
+
 while True:
     prompt=input("me: ")
     messages.append(HumanMessage(content=prompt))
-    if prompt == "1":
+    if prompt == "0":
         break 
     response=model.invoke(messages)
     messages.append(AIMessage(content=response.content))
